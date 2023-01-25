2a098f490b89b5bd9e3cdb0207acd312b5aa8cea
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-01-24 21:23:04 -0500

    almost done JS functionality. Need to figure out styling

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,21 @@
 //Math.floor(Math.random() * (max - min + 1) + min);
 let question = document.querySelector("#question");
 let multipleChoiceList = document.querySelector("#multipleChoice");
+let validate = document.querySelector("#validate");
 let btnStartQuiz = document.querySelector("#startBtn");
+let timeCount = document.querySelector("#timeCount");
+let score = document.querySelector("#score");
+let highscores = document.querySelector("#highscores");
+let initials = document.querySelector("#initials");
+let hsList = document.querySelector("#hsList");
+let replaceBtn;
+let i = 0;
+let count = 60;//////////////change back to 60/////////////////
+let indexReplace;
+let answer;
+let highscoreInitials = [];
+let highscoreCount = [];
+let timerID;
 
 let multipleChoiceOptions = [
     "if statements",
@@ -11,7 +25,8 @@ let multipleChoiceOptions = [
     "global scope",
     "key value pairs",
 ];
-let questionListArray = [1, 2, 3, 4, 5];
+
+let questionListArray = [1, 2, 3, 4, 5]; //is directly related to questionList object. could make it more flexible but for the sake of time and effort, i'll just leave it as an array
 
 let questionList = {
     1: ["_____ adds interactivity to a static website", "JavaScript"],
@@ -21,7 +36,8 @@ let questionList = {
     5: ["String values must be enclosed within _____ when being assigned to variables", "quotes"],
 };
 
-let randomOrder = function (arr) {
+///////////////////////Randomizer function///////////////////////////////////////////
+let randomOrder = function (arr) { //will randomize the order of what ever array you put in its parameters
     let b = 0;
     let c = 0;
     for (a = 0; a < arr.length; a++) {
@@ -36,40 +52,162 @@ let randomOrder = function (arr) {
 randomOrder(questionListArray); //put into a function later
 console.log(questionListArray);
 
+
+/////////////////////////////displays the question/////////////////////////////////////////
 //display the question on the screen based on the random order
-//should remove the i into another part
+//should remove the i into another part so i updates only after the user answers
+//when starting big loop, reset everything but keep track of the timer
+let displayQuestion = function () {
 
-for (let i = 0; i < questionListArray.length; i++) {
-    let index = questionListArray[1];
-    question.textContent = questionList[index][0];
-    multipleChoiceOptions = randomOrder(multipleChoiceOptions); //shuffling the options from multipleChoiceOptions
+    if (i < questionListArray.length) {
+        let index = questionListArray[i]; //i-th question on our list
+        question.textContent = questionList[index][0]; //displaying the question on the page
+        answer = questionList[index][1]; //choosing the correct answer from the question list
 
-    //need to print 4 random values from multiplechoiceoptions
-    for (let j = 0; j < 4; j++) {
-        let list = document.createElement("button"); //creating 4 buttons
-        list.textContent = multipleChoiceOptions[j]; //changing the text to MCO array
-        list.setAttribute("id", j); //gives <button data-index="0"> , 1,2,3 in this case because of the j for loop
-        multipleChoiceList.appendChild(list);
-        console.log(list);
+
+        multipleChoiceOptions = randomOrder(multipleChoiceOptions); //shuffling the options from multipleChoiceOptions (answers)
+        console.log(question);
+        displayAnswers();
+    } else {
+        finish();
     }
-    let answer = questionList[index][1]; //choosing the correct answer from the question list
-    let indexReplace = Math.floor(Math.random() * 4); //choosing a random spot in the options
-    let replaceBtn = document.getElementById(indexReplace);
-    replaceBtn.textContent = answer; //finding the index, deleting one, and replacing it with the correct answer
-    console.log(multipleChoiceOptions);
 }
 
+////////////////displays the multiple Choice answers//////////////////////
+let displayAnswers = function () {
+    //need to print 4 random values from multiplechoiceoptions
+    for (let j = 0; j < 4; j++) { //looping the process 4 times
+        let list = document.createElement("button"); //creating buttons
+        list.textContent = multipleChoiceOptions[j]; //changing the text to MCO array[j]
+        list.setAttribute("id", j); //gives <button id="0"> , 1,2,3 in this case because of the j for loop
+        multipleChoiceList.appendChild(list); //displaying the "answers" on the page (attacking to "multipleChoice ul")
+    }
+
+    indexReplace = Math.floor(Math.random() * 4); //choosing a random spot in the options
+    replaceBtn = document.getElementById(indexReplace); //using the random spot and assigning it to replaceBtn
+    replaceBtn.textContent = answer; //changing the text content of that button to the correct answer
+}
+
+
+//////////////////////////////checking if the answer is correct/////////////////////////////
+multipleChoiceList.addEventListener("click", function (event) { //listens if buttons are pressed
+    let userAns = event.target; //setting userAns to whatever was pressed
+
+    if (userAns == replaceBtn) {
+        validate.textContent = "Correct!";
+    } else {
+        validate.textContent = "Wrong!";
+        count = count - 10;
+    }
+
+    setTimeout(() => {  //give it a pause so the viewer can see if it was right or wrong
+        validate.textContent = "";
+
+        for (let c = 3; c >= 0; c--) { //removing the multiple choice answers
+            multipleChoiceList.children[c].remove();
+        }
+
+        i++; //index for the next question
+        displayQuestion(); //calling the function to display the question
+
+    }, "500");
+})
+
+//////////////////////////All done page//////////////////////////////
+let finish = function () {
+    clearInterval(timerID);
+    document.getElementById("quiz").setAttribute("style", "display: none");
+    document.getElementById("finish").setAttribute("style", "display: unset");
+    score.textContent = count;
+    storeHighScore();
+}
+
+let storeHighScore = function () {
+    document.querySelector("#submitBtn").addEventListener("click", function (event) {
+        event.preventDefault()
+        highscoreCount = JSON.parse(localStorage.getItem("hi-scores")) || [];
+
+        let newScore = {
+            initials: initials.value,
+            score: count
+        };
+
+        highscoreCount.push(newScore);
+        localStorage.setItem("hi-scores", JSON.stringify(highscoreCount));
+        highscoresPage();
+    })
+}
+
+///////////////////////////HighScores page///////////////////
+let highscoresPage = function () {
+    clearInterval(timerID);
+    document.getElementById("intro").setAttribute("style", "display: none");
+    document.getElementById("quiz").setAttribute("style", "display: none");
+    document.getElementById("finish").setAttribute("style", "display: none");
+    highscores.setAttribute("style", "display: unset");
+
+    let myHighscores = JSON.parse(localStorage.getItem("hi-scores")) || [];
+    myHighscores.sort(function (x, y) {
+        return y.score - x.score;
+    })
+
+    hsList.innerHTML = "";
+    for (let a = 0; a < myHighscores.length; a++) {
+        let li = document.createElement("li");
+        li.textContent = myHighscores[a].initials + " -- " + myHighscores[a].score;
+        hsList.appendChild(li);
+    }
+
+
+    //localStorage.getItem(initials);
+
+    document.querySelector("#backBtn").addEventListener("click", function () {
+        highscores.setAttribute("style", "display: none");
+        document.getElementById("intro").setAttribute("style", "display:unset;");
+        window.location.reload();
+    })
+
+    document.querySelector("#clearBtn").addEventListener("click", function () {
+        localStorage.removeItem("hi-scores");
+    })
+
+}
+
+
+///////////////////////if Highscores page is clicked take them to highscores page/////////
+document.querySelector("a").addEventListener("click", function (event) {
+    if (event.target) {
+        highscoresPage();
+    }
+})
+
+//////////////////////////////timer function///////////////////////////////////////
+function timer() {
+    timerID = setInterval(function () {
+        if (count <= 0) {
+            timeCount.textContent = "times up";
+            count = 0;
+            finish();
+            return;
+        }
+        timeCount.textContent = count;
+        count--;
+    }, 1000);
+
+}
+
+
+//////////////////////////////////when start is clicked///////////////////////////////////
 //when start button is clicked, the quiz begins
+//hides the intro section and shows the quiz section
+//starts the timer
 btnStartQuiz.addEventListener("click", function (event) {
     document.getElementById("intro").setAttribute("style", "display:none;");
     document.getElementById("quiz").setAttribute("style", "display: unset");
-    //console.log(randomOrder());//call a function that randomizes a question for the user
-    //spit out a question
+    displayQuestion();
+    timer();
 });
 
-let randomQuestion = function () { };
-
-multipleChoiceList;
 
 //object with 5 questions and associated answers
 
